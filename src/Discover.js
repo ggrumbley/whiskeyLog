e8644423da36580214bb57aed940e65ae8cc9461
gary <ggrumbley@ucsd.edu>
2016-08-22 11:13:15 -0700

    Button and suggest components added

---
+++ src/Discover.js
@@ -0,0 +1,35 @@
+import Logo from './components/Logo';
+import Button from './components/Button';
+import Suggest from './components/Suggest';
+import React, { Component } from 'react';
+
+class Discover extends Component {
+  render() {
+    return (
+      <div style={ {padding: '20px'} }>
+        <h1>Component discoverer</h1>
+
+        <h2>Logo</h2>
+        <div style={ {display: 'inline-block', background: 'purple'} }>
+          <Logo/>
+        </div>
+
+        <h2>Buttons</h2>
+        <div>Button with onClick: <Button onClick={() => alert('ouch')}>Click me</Button>
+        </div>
+        <div>A link: <Button href='http://reactjs.com'>Follow me</Button>
+        </div>
+        <div>
+          Custom class name:
+          <Button className="custom">I do nothing</Button>
+        </div>
+
+        <h2>Suggest</h2>
+        <div><Suggest options={['eenie', 'meenie', 'miney', 'mo']}/></div>
+
+      </div>
+    );
+  }
+}
+
+export default Discover;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Discover from './Discover';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <App />
+    <Discover />
+  </div>,
   document.getElementById('root')
 );
